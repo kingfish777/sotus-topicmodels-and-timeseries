cfb5e46424382f29fd30de197f5d5976f0b4c8ec
Kingfish Threesevens <scott.malec@gmail.com>
2014-02-17 22:26:37 -0500

    Update SOTUS_Hclust.R

--- SOTUS_Hclust.R
+++ SOTUS_Hclust.R
@@ -15,14 +15,15 @@ corpus <- tm_map(corpus, stripWhitespace)
 corpus <- tm_map(corpus, stemDocument, language = "english")
 #ngrams <- RWeka::NGramTokenizer(corpus, Weka_control(min=1, max=4))
 #dtm <- DocumentTermMatrix(corpus, control = list(ngrams, wordLengths=c(3, 25), weighting = weightTfIdf, stopwords=TRUE))
-dtm <- create_matrix(cbind(as.vector(corpus)), language="english", minDocFreq=1, maxDocFreq=Inf, 
-              minWordLength=3, maxWordLength=Inf, ngramLength=2, originalMatrix=NULL, 
-              removeNumbers=FALSE, removePunctuation=TRUE, removeSparseTerms=0, 
-              removeStopwords=TRUE,  stemWords=FALSE, stripWhitespace=TRUE, toLower=TRUE, 
-              weighting=weightTf)
+dtm <- create_matrix(cbind(as.vector(corpus)), language="english", minDocFreq=1, maxDocFreq=Inf,
+                     minWordLength=3, maxWordLength=Inf, ngramLength=2, originalMatrix=NULL,
+                     removeNumbers=FALSE, removePunctuation=TRUE, removeSparseTerms=0,
+                     removeStopwords=TRUE, stemWords=FALSE, stripWhitespace=TRUE, toLower=TRUE,
+                     weighting=weightTf)
 rowTotals <- apply(dtm , 1, sum) #Find the sum of words in each Document
-dtm   <- dtm[rowTotals> 0]           #remove all docs without words
-dtm <- removeSparseTerms(dtm, .95)
+dtm <- dtm[rowTotals> 0] #remove all docs without words
+dtm <- removeSparseTerms(dtm, .98)
+dtm_complete = hclust(dist(dtm), method="complete")
 plot(hclust(dist(dtm), method="complete"), xlab="text from corpus", "ylab"="distance", main="Cluster Dendrogram of Various Texts")
 op = par(bg="#DDE3CA")
 plot(dtm_complete, col="#487AA1", col.main="#45ADA8", col.lab="#7C8071",
@@ -32,6 +33,6 @@ plot(dtm_complete, col="#487AA1", col.main="#45ADA8", col.lab="#7C8071",
 par(op)
 nplot(dtm_complete, hang=1, axes = TRUE, ann=TRUE, main = "Cluster Dendrogram Representing Author Similarity",
       xlab="Author/Publication", ylab = "Distance")
-phyl <- as.phylo(hclust(dtm_distro))
+phyl <- as.phylo(hclust(dtm_complete))
 plot(phyl, edge.col=c("blue", "green", "red")[c(TRUE, FALSE) + 1 + (phyl$edge.length > 20)])
 ############
