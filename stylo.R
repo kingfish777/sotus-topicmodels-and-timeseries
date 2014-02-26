588cb2b4ac92f316cd0803fab094042fca0f44d4
Kingfish Threesevens <scott.malec@gmail.com>
2014-02-26 15:26:34 -0500

    Create stylo.R

---
+++ stylo.R
@@ -0,0 +1,2 @@
+library(stylo)
+stylo(gui=TRUE, frequencies=NULL, parsed.corpus=NULL, features=NULL, path=NULL, corpus.format="Plain", corpus.dir="/home/hinckley/Public/corpora/SOTUS/neoliberal_era")
